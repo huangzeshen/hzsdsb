6bc9786fc9fcf032094faba83f4b9babb6ed4023
huangzeshen <32032233+huangzeshen@users.noreply.github.com>
2017-12-26 10:03:57 +0800

    3

--- BackgroundLayer.h
+++ BackgroundLayer.h
@@ -1,12 +1,15 @@
 #pragma once
-#include"cocos2d.h"
-/*±≥æ∞≤„¿‡*/
-class BackgroundLayer :public cocos2d::Layer
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class BackgroundLayer :
+	public CCLayer
 {
 public:
-	BackgroundLayer();
-	~BackgroundLayer();
-	bool init();
-	
+	BackgroundLayer(void);
+	CREATE_FUNC(BackgroundLayer)
+	virtual bool init();
+	virtual ~BackgroundLayer(void);
 };
 
--- FishLayer.cpp
+++ FishLayer.cpp
@@ -1,20 +1,55 @@
 #include "FishLayer.h"
+#include "Fish.h"
 
-
-FishLayer::FishLayer()
-{
-}
-
-
-FishLayer::~FishLayer()
+FishLayer::FishLayer(void)
 {
 }
 
 bool FishLayer::init()
 {
-	if (!cocos2d::Layer::init())
+	do 
 	{
-		return false;
+		if (!CCLayer::init())
+		{
+			return false;
+		}
+		srand(time(0));
+		_fishes = CCArray::createWithCapacity(FISH_MAX_COUNT);
+		CC_SAFE_RETAIN(_fishes);
+		for(int i = 0; i < FISH_MAX_COUNT; i++){
+			int type = (rand()%100/(double)101) * k_Fish_Type_Count - 1;
+			Fish* fish = Fish::create((FishType)type);
+			_fishes->addObject(fish);
+		}
+		this->schedule(schedule_selector(FishLayer::addFish), 3.0f);
+		return true;
+	} while (0);
+	return false;
+}
+
+void FishLayer::addFish(float delta)
+{
+	int addToCount = (rand()%100/(double)101) * 5 + 1;
+	int count = 0;
+	CCObject* obj;
+	CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+	CCARRAY_FOREACH(_fishes, obj)
+	{
+		Fish* fish = (Fish*)obj;
+		if(fish->isRunning())
+		{
+			continue;
+		}
+		this->addChild(fish);
+		fish->setPosition(ccp((rand()%100/(double)101) * winSize.width, (rand()%100/(double)101) * winSize.height));
+		count++;
+		if(count == addToCount)
+		{
+			break;
+		}
 	}
-	return true;
+}
+
+FishLayer::~FishLayer(void)
+{
 }
