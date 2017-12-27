8be3070fa7c227720ecde8c066b75a5ffdb8abb3
huangzeshen <32032233+huangzeshen@users.noreply.github.com>
2017-12-27 20:46:55 +0800

    4

--- GameScene.h
+++ GameScene.h
@@ -3,20 +3,38 @@
 #include "BackgroundLayer.h"
 #include "FishLayer.h"
 #include "MenuLayer.h"
-USING_NS_CC;
+#include "CannonLayer.h"
+#include "TouchLayer.h"
+#include "Fish.h"
+#include "PanelLayer.h"
+#include "FishJoyData.h"
 
+USING_NS_CC;
+/*游戏场景类*/
 class GameScene :
 	public CCScene
 {
 public:
 	GameScene(void);
 	CREATE_FUNC(GameScene)
+	/*初始化函数，完成各个层的创建和加载*/
 	virtual bool init();
 	virtual ~GameScene();
+	void cannonAimAt(CCPoint target);
+	void cannonShootTo(CCPoint target);
 protected:
-	BackgroundLayer* _backgroundLayer;
-	FishLayer* _fishLayer;
-	MenuLayer* _menuLayer;
+	/*数据成员*/
+	BackgroundLayer* _backgroundLayer;/*背景层*/
+	FishLayer* _fishLayer;/*鱼层*/
+	MenuLayer* _menuLayer;/*菜单层*/
+	CannonLayer* _cannonLayer;/*炮台层*/
+	TouchLayer* _touchLayer;/*触摸层*/
+	PanelLayer* _paneLayer;/*功能层*/
 	void preloadResources(void);
+	bool checkOutCollisionBetweenFishesAndBullet(Bullet* bullet);
+	void checkOutCollision();
+	virtual void update(float delta);
+	void fishWillBeCaught(Fish* fish);
+	void checkOutCollisionBetweenFishesAndFishingNet(Bullet* bulet);
 };
 
